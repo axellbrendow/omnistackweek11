59ee3a171a6c756e7563511241ce0da2cf516180
axell-brendow <breno.axel@gmail.com>
2020-03-29 18:46:25 -0300

    Remove some .ts import errors

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -10,11 +10,43 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 2018,
     sourceType: "module"
   },
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"]
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"]
+      }
+    }
+  },
   plugins: ["@typescript-eslint", "prettier"],
   rules: {
-    "prettier/prettier": "error"
-  }
+    "prettier/prettier": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [2, { args: "none" }]
+      }
+    }
+  ]
 };
