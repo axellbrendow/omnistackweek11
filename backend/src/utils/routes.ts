3e91eab1a27b629a525adeb6773be721d5f6c3ef
axell-brendow <breno.axel@gmail.com>
2020-04-05 14:07:43 -0300

    Add validators as middlewares

--- backend/src/utils/routes.ts
+++ backend/src/utils/routes.ts
@@ -2,17 +2,32 @@ import express from "express";
 import NGOController from "../controllers/NGOController";
 import IncidentController from "../controllers/IncidentController";
 import SessionController from "../controllers/SessionController";
+import ngoValidators from "../validators/ngo";
+import sessionValidators from "../validators/session";
+import incidentValidators from "../validators/incident";
 
 const routes = express.Router();
 
-routes.route("/sessions").post(SessionController.create);
-routes.route("/ngos").post(NGOController.create).get(NGOController.list);
-routes.route("/ngos/incidents").get(NGOController.listIncidents);
+routes
+  .route("/sessions")
+  .post(sessionValidators.create.middleware, SessionController.create);
+
+routes
+  .route("/ngos")
+  .post(ngoValidators.create.middleware, NGOController.create)
+  .get(ngoValidators.list.middleware, NGOController.list);
+
+routes
+  .route("/ngos/incidents")
+  .get(ngoValidators.listIncidents.middleware, NGOController.listIncidents);
+
 routes
   .route("/incidents")
-  .post(IncidentController.create)
-  .get(IncidentController.list);
+  .post(incidentValidators.create.middleware, IncidentController.create)
+  .get(incidentValidators.list.middleware, IncidentController.list);
 
-routes.route("/incidents/:id").delete(IncidentController.delete);
+routes
+  .route("/incidents/:id")
+  .delete(incidentValidators.remove.middleware, IncidentController.delete);
 
 export default routes;
