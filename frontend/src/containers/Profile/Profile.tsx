d1260c9f74a1399a33efae525007cac4ad4e7f69
axell-brendow <breno.axel@gmail.com>
2020-04-05 14:13:26 -0300

    Add pagination and a button to fetch more cases

--- frontend/src/containers/Profile/Profile.tsx
+++ frontend/src/containers/Profile/Profile.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiPower, FiTrash2 } from 'react-icons/fi';
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
 
-import './styles.css';
+import "./styles.css";
 
-import logoImg from '../../assets/logo.svg';
-import api from '../../services/api';
+import logoImg from "../../assets/logo.svg";
+import api from "../../services/api";
 
 class Incident {
   public id!: number;
@@ -25,22 +25,36 @@ class Incident {
 
 const Profile = () => {
   const [incidents, setIncidents] = useState<Incident[]>([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
   const history = useHistory();
 
-  const ngoId = localStorage.getItem('ngoId');
-  const ngoName = localStorage.getItem('ngoName');
+  const ngoId = localStorage.getItem("ngoId");
+  const ngoName = localStorage.getItem("ngoName");
+
+  const fetchIncidents = async () => {
+    if (page > 1 && incidents?.length === total) {
+      alert("No more cases");
+      return;
+    }
+
+    const response = await api.get("/ngos/incidents", {
+      headers: {
+        Authorization: ngoId,
+      },
+      params: {
+        page,
+      },
+    });
+
+    setTotal(response.data.count);
+    setPage(page + 1);
+    setIncidents([...incidents, ...response.data.rows]);
+  };
 
   useEffect(() => {
-    (async () => {
-      const response = await api.get('/ngos/incidents', {
-        headers: {
-          Authorization: ngoId,
-        },
-      });
-
-      setIncidents(response.data);
-    })();
-  }, [ngoId]);
+    fetchIncidents();
+  }, []);
 
   const handleDeleteIncident = async (id: number) => {
     try {
@@ -50,7 +64,7 @@ const Profile = () => {
         },
       });
     } catch (error) {
-      alert('Error when deleting case, try again.');
+      alert("Error when deleting case, try again.");
     }
 
     setIncidents(incidents.filter((incident) => incident.id !== id));
@@ -58,26 +72,22 @@ const Profile = () => {
 
   const Logout = () => {
     localStorage.clear();
-    history.push('/');
+    history.push("/");
   };
 
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Logo Be The Hero" />
-        <span>
-          Welcome,
-          {' '}
-          {ngoName}
-        </span>
+        <span>Welcome, {ngoName}</span>
         <Link to="/incidents/new" className="button">
           Register new case
         </Link>
         <button type="button" onClick={Logout}>
-          <FiPower size={18} color="#e02041" />
+          <FiPower size={18} color="#e02041" title="Logout" />
         </button>
       </header>
-      <h1>Registered cases</h1>
+      <h1>Registered cases (total: {total})</h1>
       <ul>
         {incidents.map((incident) => (
           <li key={incident.id}>
@@ -89,18 +99,24 @@ const Profile = () => {
 
             <strong>VALUE:</strong>
             <p>
-              {Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
+              {Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
               }).format(incident.value)}
             </p>
 
-            <button type="button" onClick={() => handleDeleteIncident(incident.id)}>
+            <button
+              type="button"
+              onClick={() => handleDeleteIncident(incident.id)}
+            >
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
         ))}
       </ul>
+      <button type="button" className="button" onClick={fetchIncidents}>
+        Fetch more cases
+      </button>
     </div>
   );
 };
